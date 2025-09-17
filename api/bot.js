b3c428807a9beb87780761dfeab1790a283b0c83
Vagrant55 <121515731+Vagrant55@users.noreply.github.com>
2025-09-17 11:35:03 +0300

    Update bot.js

--- api/bot.js
+++ api/bot.js
@@ -10,7 +10,7 @@ export default async function handler(req, res) {
 
   const { message, callback_query } = req.body;
 
-  // 🔑 Настройки —  замените на свои
+  // 🔑 Настройки —  замените на свои 
   const TOKEN = "7991590846:AAHp6H7VW_dPhH3tf_zAjTj8aQSCYZcm6iU";
   const ADMIN_CHAT_IDS = [935264202]; // ← Добавьте chat_id админов через запятую
   const SPREADSHEET_ID = "1utCG8rmf449THR5g6SHvSK4pp6-nj7UEgSgP4H1_isc";
